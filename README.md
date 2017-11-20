3dc0f95beafbf1daf43e0d866a2b1083780bc6d3
Javier Leandro Arancibia <arancibiajav@gmail.com>
2017-11-20 20:13:01 +0100

    Last version detected from folder

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# STEPS
+## STEPS
 
 sudo docker build . -t javimosch/electron-releases
+
 sudo docker stop ers
+
 sudo docker rm ers
+
 sudo docker run --name ers -d -e IP=http://149.202.161.204:8009 -p 8009:8080 -v "$(pwd):/www" javimosch/electron-releases npm start
--- src/api/index.js
+++ src/api/index.js
@@ -8,6 +8,22 @@ import {
 import facets from './facets';
 import path from 'path';
 import compareVersion from 'compare-versions';
+import * as sander from 'sander';
+import fs from 'fs';
+
+function getLastVersion() {
+	let files = sander.readdirSync(process.cwd() + '/releases');
+	let max = '';
+	files = files.filter(f => f.indexOf('-mac.zip') !== -1).map(f => f.replace('slti-', '').replace('-mac.zip', ''));
+	files.forEach(f => {
+		if (!max) max = f;
+		if (compareVersion(f, max) === 1) {
+			max = f;
+		}
+	});
+	return max;
+}
+
 export default ({
 	config,
 	db
@@ -21,23 +37,28 @@ export default ({
 	}));
 
 	api.get('/download', (req, res) => {
-		var file = path.join(process.cwd(), "releases/slti-" + version+'-mac.zip');
+		let lastVersion = getLastVersion();
+		var file = path.join(process.cwd(), "releases/slti-" + lastVersion + '-mac.zip');
 		res.download(file);
 	});
 
 	api.get('/update/:version', (req, res) => {
-
+		let lastVersion = getLastVersion();
 		var IP = process.env.IP || "http://localhost:8081"
 
-		if(compareVersion(req.params.version,version)!==-1){
+		if (compareVersion(req.params.version, lastVersion) !== -1) {
 			return res.status(404).send();
 		}
 
+		var fd = fs.openSync(path.join(process.cwd(), '/releases/slti-' + lastVersion + '-mac.zip'), 'r')
+		let stat = sander.fstatSync(fd);
+
+
 		let rta = {
 			"url": IP + "/api/download",
-			"name": version,
+			"name": lastVersion,
 			"notes": "",
-			"pub_date": description
+			"pub_date": stat.mtime
 		};
 
 		res.json(rta);
